d69462bd4ae80cdb6be968a3c5797cdc4a7d67bf
GreetyCr <randallgbc@gmail.com>
2023-09-27 01:01:05 -0600

    Init commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import BookInfo from "./pages/BookInfo";
+import { books } from "./data";
+import Cart from "./pages/Cart";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book => book.id !== item.id))
+    console.log("remove item", item);
+  }
+
+  function numberOfItems() {
+    let counter = 0;
+    cart.forEach(item => {
+      counter += item.quantity
+    })
+    return counter;
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
+  return (
+    <Router>
+      <div className="App">
+        <Nav numberOfItems={numberOfItems()}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/books" render={() => <Books books={books} />} />
+        <Route
+          exact
+          path="/books/:id"
+          render={() => (
+            <BookInfo books={books} cart={cart} addToCart={addToCart} />
+          )}
+        />
+        <Route
+          exact
+          path="/cart"
+          render={() => (
+            <Cart
+              books={books}
+              cart={cart}
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+            />
+          )}
+        />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,125 @@
+// FAKE DATA
+export const books = [
+  {
+    id: 1,
+    title: "Crack the Coding Interview",
+    url: "https://covers.openlibrary.org/b/id/8091016-L.jpg",
+    originalPrice: 49.95,
+    salePrice: 14.95,
+    rating: 4.5,
+    sum1: "A comprehensive guide to cracking coding interviews, this book is a treasure trove for those aspiring to land a job in top tech companies. It delves deep into the nuances of algorithms and data structures, offering readers a blend of theory and practical problem-solving techniques.",
+    sum2: "With real interview questions from giants like Google, Amazon, and Microsoft, this book is a testament to the author's experience and expertise. It not only teaches how to solve problems but also how to think algorithmically, making it an indispensable resource for every software engineer."
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    url: "https://covers.openlibrary.org/b/id/10958382-L.jpg",
+    originalPrice: 39,
+    salePrice: null,
+    rating: 5,
+    sum1: "Atomic Habits delves into the intricate world of habits, breaking down the science behind why they form and how they can be changed. James Clear, the author, presents a compelling case for making tiny adjustments to one's daily routine, showcasing how these can lead to transformative results over time.",
+    sum2: "Clear's writing is backed by scientific research, personal anecdotes, and real-world examples. He provides actionable insights on breaking bad habits, forming good ones, and creating an environment conducive to productive behaviors. A must-read for anyone looking to bring about positive change in their life."
+  },
+  // ... (Continuing from the previous books)
+
+{
+  id: 3,
+  title: "Can't Hurt Me",
+  url: "https://covers.openlibrary.org/b/id/10425061-L.jpg",
+  originalPrice: 29,
+  salePrice: null,
+  rating: 5,
+  sum1: "In 'Can't Hurt Me', David Goggins takes readers on a riveting journey through his extraordinary life, from his challenging childhood to his storied career as a Navy SEAL. He shares the mental and physical challenges he faced and overcame, teaching us that most of us only tap into a fraction of our potential.",
+  sum2: "Goggins' story is one of relentless self-discipline and mental toughness. He challenges readers to push past their perceived limits and embrace the power of self-belief. His tale is not just about physical endurance but also about the resilience of the human spirit."
+},
+{
+  id: 4,
+  title: "Deep Work",
+  url: "https://covers.openlibrary.org/b/id/10088428-L.jpg",
+  originalPrice: 44,
+  salePrice: 19,
+  rating: 4.5,
+  sum1: "Cal Newport's 'Deep Work' is a timely manual in our distraction-laden age. Newport argues that the ability to focus without distraction on a cognitively demanding task, which he terms 'deep work', is becoming increasingly rare, yet it's more valuable than ever in our economy.",
+  sum2: "The book provides actionable strategies for prioritizing deep work and eliminating the distractions of our digital world. Newport draws on a myriad of examples, from writers to businessmen, showcasing the profound benefits of dedicating uninterrupted time to meaningful work."
+},
+{
+  id: 5,
+  title: "The 10X Rule",
+  url: "https://covers.openlibrary.org/b/id/9978588-L.jpg",
+  originalPrice: 32,
+  salePrice: null,
+  rating: 5,
+  sum1: "Grant Cardone's 'The 10X Rule' is a call to action for anyone seeking real success. The central premise is that you should set targets that are 10 times higher than what you believe you can achieve and then do 10 times what you think is required to achieve those targets.",
+  sum2: "Cardone believes that most people operate at only a fraction of their potential. He provides a step-by-step guide on how to redefine your goals, evaluate what you'll need to reach them, and then execute your plan relentlessly. It's a blueprint for extraordinary achievement."
+},
+{
+  id: 6,
+  title: "Sell Or Be Sold",
+  url: "https://covers.openlibrary.org/b/id/7737110-L.jpg",
+  originalPrice: 70,
+  salePrice: 12.5,
+  rating: 5,
+  sum1: "Grant Cardone's 'Sell Or Be Sold' is more than just a sales book. While it does delve deep into the nuances of the sales process, it also emphasizes that selling is a fundamental skill required in all areas of life, whether you're pitching an idea, a product, or yourself.",
+  sum2: "Cardone shares his insights from decades of successful selling, teaching readers how to handle rejection, turn around negative situations, and harness the power of persuasion. He believes that mastering the art of selling can improve every aspect of your life."
+},
+{
+  id: 7,
+  title: "Rich Dad Poor Dad",
+  url: "https://covers.openlibrary.org/b/id/2380224-L.jpg",
+  originalPrice: 11,
+  salePrice: 10,
+  rating: 4,
+  sum1: "Robert Kiyosaki's 'Rich Dad Poor Dad' is a groundbreaking financial literacy book that contrasts the mindset and practices of Kiyosaki's two 'dads': his biological father (Poor Dad) and the father of his childhood best friend (Rich Dad).",
+  sum2: "Through these contrasting perspectives, Kiyosaki imparts lessons on money, work, and life. He challenges conventional beliefs about work, education, and investing, offering a new perspective on money that emphasizes financial education, smart investing, and building wealth over time."
+},
+{
+  id: 8,
+  title: "Cashflow Quadrant",
+  url: "https://covers.openlibrary.org/b/id/1954899-L.jpg",
+  originalPrice: 38,
+  salePrice: 17.95,
+  rating: 4.5,
+  sum1: "A sequel to 'Rich Dad Poor Dad', 'Cashflow Quadrant' delves deeper into Kiyosaki's revolutionary thinking about money. He introduces the concept of the four types of people who make up the business world: Employees, Self-Employed, Business Owners, and Investors.",
+  sum2: "Kiyosaki emphasizes the need to move from the left side of the quadrant (E and S) to the right side (B and I) if one wishes to achieve financial freedom. The book provides insights and strategies to make this transition, offering readers a roadmap to financial success."
+},
+{
+  id: 9,
+  title: "48 Laws of Power Summary",
+  url: "https://covers.openlibrary.org/b/id/8906626-L.jpg",
+  originalPrice: 35,
+  salePrice: 19.95,
+  rating: 4.5,
+  sum1: "A distilled version of Robert Greene's magnum opus, this summary provides readers with a concise overview of the laws governing power and influence. Greene draws from historical events and figures, illustrating how these laws have been used, and sometimes abused, throughout history.",
+  sum2: "From mastering the art of confidence to learning when to use or avoid aggression, this book is a guide to the subtle dynamics of power and manipulation. It's a manual for those seeking to achieve, maintain, or defend their power in any sphere of life."
+},
+{
+  id: 10,
+  title: "The 5 Second Rule",
+  url: "https://covers.openlibrary.org/b/id/8114155-L.jpg",
+  originalPrice: 40,
+  salePrice: null,
+  rating: 5,
+  sum1: "Mel Robbins introduces readers to a simple yet transformative tool: The 5 Second Rule. The premise is straightforward - if you have an instinct to act on a goal, you must physically move within 5 seconds or your brain will kill the idea.",
+  sum2: "Robbins shares how this rule helped her transform her life, career, and confidence. The book is filled with riveting stories, groundbreaking science, and simple tools that can help readers push past self-doubt, fear, and procrastination to achieve their goals."
+},
+{
+  id: 11,
+  title: "How to Win Friends & Influence People",
+  url: "https://covers.openlibrary.org/b/id/7895280-L.jpg",
+  originalPrice: 30,
+  salePrice: 20,
+  rating: 5,
+  sum1: "Dale Carnegie's timeless classic is a masterclass in human relations. Written in a pre-digital age, its principles remain as relevant today as they were then. Carnegie provides actionable advice on how to make friends, influence others, and win people to your way of thinking.",
+  sum2: "Drawing from a plethora of real-life examples, Carnegie showcases the power of charm, appreciation, and empathy. He teaches readers how to handle disagreements, how to be persuasive without being pushy, and how genuine interest in others can open doors to personal and professional success."
+},
+{
+  id: 12,
+  title: "Mastery",
+  url: "https://covers.openlibrary.org/b/id/8479576-L.jpg",
+  originalPrice: 30,
+  salePrice: 12.95,
+  rating: 4.5,
+  sum1: "Robert Greene's 'Mastery' is a deep dive into the process of achieving greatness in any field. Greene believes that mastery is not a result of mere talent but a balance of passion, dedication, and intuitive search for knowledge.",
+  sum2: "Through historical examples like Leonardo da Vinci, Charles Darwin, and contemporary masters like Freddie Roach, Greene illustrates the developmental phases of mastery and provides invaluable insights into the journey, highlighting the importance of mentorship, deep practice, and learning from failures."
+}
+];
